9b7a706a49c521667c67f3244be8895950a04192
Phu Duc Nguyen <sillycat@duck.com>
2023-03-14 02:48:49 +0700

    Create Player and Enemy

--- index.js
+++ index.js
@@ -8,3 +8,77 @@ canvas.height = 576
 
 // Draw a filled rectangle covering the entire canvas
 c.fillRect(0, 0, canvas.width, canvas.height)
+
+// Set the gravity constant for all sprites
+const gravity = 0.7
+
+// Define a Sprite class for creating player and enemy objects
+class Sprite {
+	constructor({ position, velocity }) {
+		this.position = position
+		this.velocity = velocity
+		this.width = 50
+		this.height = 150
+	}
+
+	// Draw the sprite as a red rectangle
+	draw() {
+		c.fillStyle = 'red'
+		c.fillRect(this.position.x, this.position.y, this.width, this.height)
+	}
+
+	// Update the sprite's position and velocity
+	update() {
+		this.draw()
+		this.position.y += this.velocity.y
+
+		// If the sprite hits the ground, set its velocity to 0
+		if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+			this.velocity.y = 0
+		} else {
+			// Otherwise, increase the sprite's velocity due to gravity
+			this.velocity.y += gravity
+		}
+	}
+}
+
+// Create a new player sprite at the top-left corner of the canvas
+const player = new Sprite({
+	position: {
+		x: 0,
+		y: 0
+	},
+	velocity: {
+		x: 0,
+		y: 0
+	}
+})
+
+// Create a new enemy sprite at the top-right corner of the canvas
+const enemy = new Sprite({
+	position: {
+		x: canvas.width - 50,
+		y: 0
+	},
+	velocity: {
+		x: 0,
+		y: 0
+	}
+})
+
+// Define the animation loop
+function animate() {
+	// Request the next animation frame
+	window.requestAnimationFrame(animate)
+
+	// Clear the canvas with a black background
+	c.fillStyle = 'black'
+	c.fillRect(0, 0, canvas.width, canvas.height)
+
+	// Update and draw the player and enemy sprites
+	player.update()
+	enemy.update()
+}
+
+// Start the animation loop
+animate()
